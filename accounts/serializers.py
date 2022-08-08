dd96430a9966baacd47c5e159d6498580a0efbcc
IiroKarppanen <iiro.karppanen@gmail.com>
2022-08-08 12:18:58 +0300

    test

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,6 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
+        print("PASSWORD")
+        print(password)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
