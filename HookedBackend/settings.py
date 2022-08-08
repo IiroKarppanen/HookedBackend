28531caa7259ba7152761be5209757da233583cd
IiroKarppanen <iiro.karppanen@gmail.com>
2022-08-08 13:10:53 +0300

    test

--- HookedBackend/settings.py
+++ HookedBackend/settings.py
@@ -91,12 +91,7 @@ WSGI_APPLICATION = 'HookedBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 #DATABASES = {
