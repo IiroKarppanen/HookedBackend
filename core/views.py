5a584eec874ebed5ec2e14c6d06be092f56b10de
IiroKarppanen <iiro.karppanen@gmail.com>
2022-08-24 12:13:17 +0300

    final commit

--- core/views.py
+++ core/views.py
@@ -7,26 +7,20 @@ import json
 
 # Load movies from json file to database if database is empty or if json file lenght has changes
 
-#with open(r"movies.json", encoding="utf8") as f:
-#    data = json.load(f)
-#
-#    print("JSON")
-#    print(len(data))#
-#
-#    print("DB")
-#    print(Movies.objects.all().count())
-#
-#    if(Movies.objects.all().count() != len(data)):
-#        print("TOIMII")
-#
-#    if(Movies.objects.all().count() == 0 or Movies.objects.all().count() != len(data)):
-#        Movies.objects.all().delete()
-#        for movie in data:
- #           serializer = MovieSerializer(data=movie)
- #           if serializer.is_valid():        
- #               serializer.save()
-    #            print("VALID")
-#
+with open(r"movies.json", encoding="utf8") as f:
+    data = json.load(f)
+
+    if(Movies.objects.all().count() != len(data)):
+        print("TOIMII")
+
+    if(Movies.objects.all().count() == 0 or Movies.objects.all().count() != len(data)):
+        Movies.objects.all().delete()
+        for movie in data:
+            serializer = MovieSerializer(data=movie)
+            if serializer.is_valid():        
+                serializer.save()
+                print("VALID")
+
 # MOVIE DATA FUNCTIONS
 
 @api_view(['GET', 'POST'])
