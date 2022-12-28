6b354b8cbdd5c962cc19a1dfcaaa370cd355261b
Oxey83 <39335935+IiroKarppanen@users.noreply.github.com>
2022-12-28 22:56:09 +0200

    Update settings.py

--- HookedBackend/settings.py
+++ HookedBackend/settings.py
@@ -35,7 +35,7 @@ SESSION_COOKIE_HTTPONLY = True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['https://hookedtomovies.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://hookedtomovies.com']
 #RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
